429d672012e968e8a08c8f676cfff8f122a529ce
tianyi.zhang <tianyi.zhang@linkflowtech.com>
2022-12-28 18:12:25 +0800

    Description:<feat> Add more test

--- src/test/java/com/st/example/HelloSecurityApplicationTests.java
+++ src/test/java/com/st/example/HelloSecurityApplicationTests.java
@@ -5,14 +5,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -49,6 +55,37 @@ public class HelloSecurityApplicationTests {
     @Test
     public void loginInvalidUser() throws Exception {
         this.mockMvc.perform(formLogin().user("invalid").password("invalid"))
+                .andExpect(unauthenticated())
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void loginUserAccessProtected() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(formLogin().user("root").password("root"))
+                .andExpect(authenticated())
+                .andReturn();
+        HttpSession session = mvcResult.getRequest().getSession(false);
+        assert session != null;
+        Enumeration<String> names = session.getAttributeNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            log.warn("session name: {} value:{}", name, session.getAttribute(name));
+        }
+        this.mockMvc.perform(get("/user/index").session((MockHttpSession) session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void logout() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(formLogin().user("root").password("root"))
+                .andExpect(authenticated())
+                .andReturn();
+        HttpSession session = mvcResult.getRequest().getSession(false);
+
+        assert session != null;
+        this.mockMvc.perform(post("/logout").with(csrf()).session((MockHttpSession) session))
+                .andExpect(unauthenticated());
+        this.mockMvc.perform(get("/user/index").session((MockHttpSession) session))
                 .andExpect(unauthenticated())
                 .andExpect(status().is3xxRedirection());
     }
